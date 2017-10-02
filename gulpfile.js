f60eeef3b2a973d77804994547a2a1bc92d53ffe
svetoldo4444ka <svitlana.korotkykh@easternpeak.com>
2017-10-03 00:15:38 +0300

    change footer styles, change gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,24 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync').create(),
-    cleanCSS = require('gulp-clean-css'),
     autoprefixer = require('gulp-autoprefixer'),
     spritesmith = require('gulp.spritesmith');
 
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    })
+});
+
 gulp.task('sass', function(){
-    return gulp.src('src/sass/**/*.scss')
+    return gulp.src('src/sass/**/*.scss') // Берем все sass файлы из папки sass и дочерних, если таковые будут
         .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(cleanCSS())
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.reload({
             stream: true
@@ -30,8 +36,7 @@ gulp.task('sprite', function () {
     return spriteData.pipe(gulp.dest('src/images/sprite'));
 });
 
-
 gulp.task('watch', ['browserSync', 'sass'], function() {
-    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('src/sass/**/*.scss', ['sass']); //
     gulp.watch('src/*.html', browserSync.reload);
 });
